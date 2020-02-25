d269591745d2ca06e2b8efa3394171141477ce21
haleonas <altes@live.se>
2020-02-25 11:12:35 +0100

    Initial Commit

---
+++ Labb2Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Labb2Swift
+//
+//  Created by Jesper Stenlund on 2020-02-25.
+//  Copyright © 2020 Jesper Stenlund. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
